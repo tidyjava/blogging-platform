71bc2ee3e22c75a9e5504b5352be8027dcd9d505
Grzegorz Ziemoński <grzegorz.ziemonski@gmail.com>
2016-07-12 19:47:52 +0200

    Configure Gradle, Travis, Spring Boot and Spock

---
+++ src/main/resources/templates/home.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello name</title>
+</head>
+
+<body>
+Hello ${name}
+</body>
+
+</html>
---
+++ src/test/groovy/com/tidyjava/bp/HomeControllerSpec.groovy
@@ -0,0 +1,14 @@
+package com.tidyjava.bp
+
+import spock.lang.Specification
+
+class HomeControllerSpec extends Specification {
+
+    def "test"() {
+        given:
+        println("Test running again!")
+
+        expect:
+        true
+    }
+}
