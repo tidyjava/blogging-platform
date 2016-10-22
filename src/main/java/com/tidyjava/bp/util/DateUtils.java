097faa33445ef248217cde9c350a139858cfcd96
Grzegorz Ziemoński <grzegorz.ziemonski@gmail.com>
2016-10-22 22:33:36 +0200

    Use system default zoneId for date conversions to avoid timezone issues with sitemap

--- src/main/java/com/tidyjava/bp/util/DateUtils.java
+++ src/main/java/com/tidyjava/bp/util/DateUtils.java
@@ -3,11 +3,11 @@ package com.tidyjava.bp.util;
 import java.time.LocalDate;
 import java.util.Date;
 
-import static java.time.ZoneOffset.UTC;
+import static java.time.ZoneId.systemDefault;
 
 public class DateUtils {
 
     public static Date toDate(LocalDate localDate) {
-        return Date.from(localDate.atStartOfDay().atOffset(UTC).toInstant());
+        return Date.from(localDate.atStartOfDay().atZone(systemDefault()).toInstant());
     }
 }
